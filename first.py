7316ed859c9f12fe14853e94d6102aaf51f171b7
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-14 14:37:04 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,13 @@
+print("My Name is krishna.")
+print("My Age is 22.")
+print("I am from India")
+print("I like to play football.")
+ 
+ 
+def add(a, b):
+    
+    return a + b
+
+print(add(5, 6))
+
+
