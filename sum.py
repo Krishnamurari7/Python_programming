27ed402a14529613381253e3d411031ba57c988e
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-18 07:37:07 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+#sum of two numbers
+def sum(a, b):
+
+    return a + b
+    # print("The sum is", sum)
+
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+sum_of_two_numbers = sum(num1, num2)
+print(sum_of_two_numbers)
+    
