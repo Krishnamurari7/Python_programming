c88cb86c533865c17be5a0ad2fa147a4c526bcc8
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-30 13:34:40 +0530

    New program

---
+++ Arthmetic.py
@@ -0,0 +1,17 @@
+a = 63
+b = 31
+
+print(f"Added the two numbers :-{a + b}")  # Added
+
+print(f"Subtract the two numbers :-{a - b}")  # Substraction
+
+print(f"Multiply the two numbers :-{a * b}") # multiplication
+
+print(f"Divided the two numbers :-{a / b}") #Check Divisior and answer float Format
+
+print(f"Modula the two numbers :-{a % b}") #Check Remainder
+
+print(f"Power :- {a ** b}") #63 ka Power 31
+
+print(f"Divided the Two number strinc Mode no floating Point :-{a // b}") #Check Divisior and answer integer Format
+
