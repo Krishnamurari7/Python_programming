b656e1c9599221f2498e77fbfc6ff85d383c0da8
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-30 13:44:31 +0530

    update

--- Comparison.py
+++ Comparison.py
@@ -3,15 +3,15 @@
 a = 45
 b = 32
 
-print(f"Greater than :-{a > b}")  # Greater than
+print(f"Greater than :-{a > b}")  # Greater than 45 > 32 True
 
-print(f"Less than :-{a < b}")  # Less than
+print(f"Less than :-{a < b}")  # Less than  45 < 32 false
 
-print(f"Equal to :-{a == b}")  # Equal to
+print(f"Equal to :-{a == b}")  # Equal to 45 == 32 false
 
-print(f"Not Equal to :-{a != b}")  # Not Equal to
+print(f"Not Equal to :-{a != b}")  # Not Equal to 45 != 32 true
 
-print(f"Greater than or Equal to :-{a >= b}")  # Greater than or Equal to
+print(f"Greater than or Equal to :-{a >= b}")  # Greater than or Equal to 45 >= 32 true
 
-print(f"Less than or Equal to :-{a <= b}")  # Less than or Equal to
+print(f"Less than or Equal to :-{a <= b}")  # Less than or Equal to 45 <= 32 false
 
