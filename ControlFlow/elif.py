2b3a7d845a638a2bbe6ee12ff5e985f151f2d9c9
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-30 14:17:33 +0530

    new program elif

---
+++ ControlFlow/elif.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+
+if marks > 80 :
+    print("Your grade is A+")
+elif marks > 60 :
+    print("your grade is A")
+elif marks > 40 :
+    print("your grade is B")
+elif marks > 30 :
+    print("your grade is C")
+else :
+    print("you failed")
