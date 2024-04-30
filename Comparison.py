61eda57e879a3c24322d734da7309f9612deda0a
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-30 13:40:14 +0530

    Create Comparison.py

---
+++ Comparison.py
@@ -0,0 +1,17 @@
+#Comaprison Operatior always Answer Boolean Format
+
+a = 45
+b = 32
+
+print(f"Greater than :-{a > b}")  # Greater than
+
+print(f"Less than :-{a < b}")  # Less than
+
+print(f"Equal to :-{a == b}")  # Equal to
+
+print(f"Not Equal to :-{a != b}")  # Not Equal to
+
+print(f"Greater than or Equal to :-{a >= b}")  # Greater than or Equal to
+
+print(f"Less than or Equal to :-{a <= b}")  # Less than or Equal to
+
