6165cb00aae13d1d8967ccd1104e0006346afd67
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-01 21:55:50 +0530

    new program

---
+++ gender.py
@@ -0,0 +1,8 @@
+gender = input("enter your gender:-")
+
+if gender == "male":
+    print("good morning sir")
+elif gender == "female":
+    print("good morning mam ")
+else:
+    print("not male and not female")
