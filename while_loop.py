353b93b805cc96b5908c538319cb533c01cbd0e7
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-01 13:48:09 +0530

    new program while loop

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+# words = ["intelligent","good","better","best","good","better","best","good","better","best"]
+
+# for i in words:
+   
+#     while i == "good":
+#         print(i)
+#         i += 1
+
+
+# for i in range(1,20):
+
+#     print(i)
+#     while i == 10:
+#         print("this is ten")
+#         i += 1
+
+
+a = 0
+while a < 10 :
+    print(a)
+    a += 1
