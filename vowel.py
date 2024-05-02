f33e8283af5e2d2585f8ae03dd201d5e1f236787
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-02 11:44:56 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -3,4 +3,4 @@ letter = input("Enter any letter:- ")
 if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
     print("The letter is vowel")
 else:
-    print("The letter is not vowel")
+    print("The letter is Consonent")
