cc122a34de4fde63abf4925bc89164fd3e5c197e
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-02 11:47:27 +0530

    update and easy

--- vowel.py
+++ vowel.py
@@ -1,6 +1,7 @@
 letter = input("Enter any letter:- ")
 
-if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+# if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U":
+if letter in "aeiouAEIOU":
     print("The letter is vowel")
 else:
     print("The letter is Consonent")
