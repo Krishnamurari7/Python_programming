b09155b9bc2fcb1c8fcaaaaefcc9699b142d75e9
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-02 11:44:20 +0530

    new program check vowel

---
+++ vowel.py
@@ -0,0 +1,6 @@
+letter = input("Enter any letter:- ")
+
+if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+    print("The letter is vowel")
+else:
+    print("The letter is not vowel")
