1620c5b39bd1c6ba1de5196d3c6ca2633691a5ec
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-03 18:47:23 +0530

    new program

---
+++ average.py
@@ -0,0 +1,10 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+
+sum = first_number+second_number
+
+# avg = sum//2  without float value 
+avg = sum/2  #with float value
+
+print(avg)
