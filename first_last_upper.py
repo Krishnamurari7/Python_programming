b11bb9e13b21796634d6468184a0cba4830bbaf4
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-05 19:02:56 +0530

    new program

---
+++ first_last_upper.py
@@ -0,0 +1,12 @@
+str = input("Enter any string:-")
+
+a = str.split()
+print(a)
+res = []
+for i in a:
+    x = i[0].upper()+i[1:-1]+i[-1].upper()
+    res.append(x)
+res = " ".join(res)
+print("String after:", res)
+
+    
