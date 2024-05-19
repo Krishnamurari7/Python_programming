34aac670bd209d82312558dc376fab67f2de201c
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-05-19 12:51:37 +0530

    update

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,8 @@
 year = int(input("Enter the year: "))
 
+
+
+#single line comment
 # if year % 4 == 0:
 #     if year % 400 == 0:
 #         print("it is leap year")
@@ -9,6 +12,19 @@ year = int(input("Enter the year: "))
 #         print('Leap Year')
 # else:
 #     print('Not a Leap Year')
+
+
+"""
+Multiline comment
+if year % 4 == 0:               
+    if year % 400 == 0:
+        print("it is leap year")
+    elif year % 100 == 0:
+        print('not a Leap Year')
+    else:
+        print('Leap Year')
+else:
+    print('Not a Leap Year')"""
 
 if year % 4 == 0 and year % 100 != 0:
     print('Leap Year')
