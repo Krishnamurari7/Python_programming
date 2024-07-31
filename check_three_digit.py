e6c3035481fa2751c030e40dee6a76ceed76f995
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-07-31 08:32:07 +0530

    check three digit ya not

---
+++ check_three_digit.py
@@ -0,0 +1,6 @@
+number = int(input("enter three digit number :- "))
+
+if number > 99 and number < 1000:
+    print(f"{number} is three digit number")
+else:
+    print(f"{number} is not three digit number")
