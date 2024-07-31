12a43727a975add2a8275ee07e6ca4d4249d47ed
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-07-31 08:16:51 +0530

    two value swaping

---
+++ swap.py
@@ -0,0 +1,17 @@
+first = int(input("Enter first number:-"))
+second = int(input("Enter second number:-"))
+
+print(f"first number:-{first} second number :- {second}")
+
+#without third variable
+first = first + second
+second = first - second
+first = first - second
+
+
+# with third variable
+# swap = first
+# first = second 
+# second = swap
+
+print(f"first number:-{first} second number :- {second}")
