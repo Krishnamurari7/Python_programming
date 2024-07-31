6c7955f87f8d53ab07501987a7ff9e77c89cdbf8
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-07-31 08:07:30 +0530

    simple interest

---
+++ simple_intrest.py
@@ -0,0 +1,8 @@
+principle = int(input("Enter the principle:-"))
+rate = int(input("Enter the rate:-"))
+time = int(input("Enter time:-"))
+
+
+SI = (principle*rate*time)/100
+
+print(f"Simple intrest is {SI}")
